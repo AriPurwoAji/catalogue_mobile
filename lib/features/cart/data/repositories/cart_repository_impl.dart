bd22a1e7c0212152bfb6e1e7020f78ee19a524f6
AriPurwoAji <ajiaripurwo@gmail.com>
2026-03-03 20:28:21 +0700

    add cart_repository

--- lib/features/cart/data/repositories/cart_repository_impl.dart
+++
@@ -1,8 +0,0 @@
-import 'package:catalogue/features/cart/domain/entities/product.dart';
-
-abstract class CartRepository {
-  List<Product> getCartItems();
-  void addItem(Product product);
-  void removeAllItems();
-  bool isItemInCart(String productId);
-}
