5c60a4731c6acc928689157d1240d3a7d7921766
AriPurwoAji <ajiaripurwo@gmail.com>
2026-03-03 19:10:04 +0700

    add class MyCatalog

--- lib/main.dart
+++ lib/main.dart
@@ -32,6 +32,22 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      initialRoute: '/',
+      routes: {
+        '/' :(context) => const MyCatalog(),
+        '/cart' :(context) => const MyCart(),
+      },
+    );
+  }
+}
+
+class MyCatalog extends StatelessWidget {
+  const MyCatalog({super.key});
+
+  @override
+  Widget build(BuildContext context) {
     return const Placeholder();
   }
 }
